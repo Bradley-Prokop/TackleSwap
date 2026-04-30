305f17de6126e1704abf8120771bc4be697b14f7
Brad Prokop <baproko@ilstu.edu>
2026-04-29 19:24:32 -0500

    Set up firebase config and connection to the database.

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGGqACzLW_AzZaN0wrb_S_EGrcj3qvUog",
+  authDomain: "tackle-swap-3a8d9.firebaseapp.com",
+  projectId: "tackle-swap-3a8d9",
+  storageBucket: "tackle-swap-3a8d9.firebasestorage.app",
+  messagingSenderId: "721592703255",
+  appId: "1:721592703255:web:cbcaa26013994dbbfe3f52"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//Initialize fireStore
+const db = getFirestore(app);
