ae94ac9120e067efe17fdee247aa102a89fe46b8
Brad Prokop <baproko@ilstu.edu>
2026-05-05 17:26:02 -0500

    Added logout and session handling logic.

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,17 @@ import Home from './pages/views/Home';
 import NavBar from './components/navbar/NavBar';
 import MyTrades from './pages/views/MyTrades';
 import Messages from './pages/views/Messages';
+import { useLocation } from "react-router-dom";
 
 function App() {
+
+  const location = useLocation();
+  const hideNavbarList = ["/login", "/signup"];
+  const hideNavBar = hideNavbarList.includes(location.pathname);
+
   return ( 
-    <BrowserRouter>
-      <NavBar/>
+    <>
+      {!hideNavBar && <NavBar/>}
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
@@ -17,7 +23,7 @@ function App() {
         <Route path='/my-trades' element={<MyTrades/>}/>
         <Route path='/messages' element={<Messages/>}/>
       </Routes>
-    </BrowserRouter>
+    </>
   );
 };
 
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,22 +1,43 @@
 import "./Navbar.css";
 import logoImg from "../../assets/navbar/logo.png";
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../config/firebase";
 
 export default function NavBar() {
+  const { user, loading } = useAuth();
+  const navigation = useNavigate();
 
+  if (loading) {
     return (
-        <div className="navbar-container">
-            
-            <img src={logoImg} alt="TackleSwap logo" />
-
-            <div className="nav-links">
-                <NavLink to="/" end className="nav-link">Home</NavLink>
-                <NavLink to="/my-trades" className="nav-link">My Trades</NavLink>
-                <NavLink to="/messages" className="nav-link">Messages</NavLink>
-            </div>
-
-            <button>Login/Sign Up</button>
-        </div>
+      <div className="navbar-container">
+        <p>loading...</p>
+      </div>
     );
+  }
 
-};
+  return (
+    <div className="navbar-container">
+      <img src={logoImg} alt="TackleSwap logo" />
+
+      <div className="nav-links">
+        <NavLink to="/" end className="nav-link">
+          Home
+        </NavLink>
+        <NavLink to="/my-trades" className="nav-link">
+          My Trades
+        </NavLink>
+        <NavLink to="/messages" className="nav-link">
+          Messages
+        </NavLink>
+      </div>
+
+        {user ? (
+            <button onClick={()=> auth.signOut()}>Logout</button>
+        ) : (
+            <button onClick={()=> navigation("\login")}>Login/Sign Up</button>
+        )}
+    </div>
+  );
+}
