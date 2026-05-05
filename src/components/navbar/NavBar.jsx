40857db030ccf6c4f91a49c73259d230219d51e1
Brad Prokop <baproko@ilstu.edu>
2026-05-05 16:09:43 -0500

    Cleaned up CSS.

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -6,15 +6,16 @@ export default function NavBar() {
 
     return (
         <div className="navbar-container">
+            
             <img src={logoImg} alt="TackleSwap logo" />
+
             <div className="nav-links">
                 <NavLink to="/" end className="nav-link">Home</NavLink>
                 <NavLink to="/my-trades" className="nav-link">My Trades</NavLink>
                 <NavLink to="/messages" className="nav-link">Messages</NavLink>
             </div>
 
-            <button className="login">Login</button>
-            <button className="signup">Sign Up</button>
+            <button>Login/Sign Up</button>
         </div>
     );
 
