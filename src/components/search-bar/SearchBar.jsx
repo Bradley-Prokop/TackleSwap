8988689d014ef7561d8a3f28550ada79cede5aea
Brad Prokop <baproko@ilstu.edu>
2026-05-27 16:51:18 -0500

    Got search bar partially set up with results section.

--- src/components/search-bar/SearchBar.jsx
+++ src/components/search-bar/SearchBar.jsx
@@ -1,11 +1,30 @@
-import './SearchBar.css';
-import SearchBarImg from '../../assets/icons/search.svg';
+import "./SearchBar.css";
+import SearchBarImg from "../../assets/icons/search.svg";
+import { use, useState } from "react";
 
-export default function SearchBar(){
-    return(
-        <div className="search-bar-main-container">
-            <img className='search-bar-icon' src={SearchBarImg} alt="Search bar icon" />
-            <input className='search-bar-input' type="text" placeholder='What are you looking for?' />
-        </div>
-    );
+export default function SearchBar({ onSearch }) {
+  const [searchText, setSearchText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(searchText);
+  };
+
+  return (
+    <form className="search-bar-main-container">
+      <img
+        className="search-bar-icon"
+        src={SearchBarImg}
+        alt="Search bar icon"
+      />
+      <input
+        className="search-bar-input"
+        type="text"
+        placeholder="What are you looking for?"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+      <button type="submit" onClick={handleSubmit} className="search-bar-submit-btn">Search</button>
+    </form>
+  );
 }
--- src/pages/views/Browse.jsx
+++ src/pages/views/Browse.jsx
@@ -2,30 +2,35 @@ import "./Browse.css";
 import SearchBar from "../../components/search-bar/SearchBar";
 import Card from "../../components/card/Card";
 import fishingPoleIMG from "../../assets/fishing_pole.png";
-import fishingReelIMG from '../../assets/fishing_reel.png';
+import fishingReelIMG from "../../assets/fishing_reel.png";
+import { useState } from "react";
 
 export default function Browse() {
+  const [searchBarText, setSearchBarText] = useState("");
+
+  const handleSearch = (text) => {
+    setSearchBarText(text);
+  };
+
   return (
     <div className="browse-main-container">
       <div className="browse-search-bar-container">
-        <SearchBar></SearchBar>
+        <SearchBar onSearch={handleSearch}></SearchBar>
       </div>
 
-      {/* <div className="browse-recently-posted-container">
-        <h1 className="recently-posted-title">Recently Posted</h1>
-        <Card
-          title="Abu Garcia Revo X Spinning Reel"
-          image={fishingPoleIMG}
-          price="70 OBO"
-          className="recently-posted-card"
-        />
-        <Card
-          title="St. Croix Triumph Casting Rod"
-          image={fishingPoleIMG}
-          price="140 or reel trade"
-          className="recently-posted-card"
-        />
-      </div> */}
+      {searchBarText != "" && (
+        <div className="browse-search-result-container">
+          <h2 className="browse-search-result-label">
+            Showing results for "{searchBarText}"
+          </h2>
+
+          <hr className="browse-search-result-hr" />
+
+          <div className="browse-search-results">
+            {/* dynamically render results here */}
+          </div>
+        </div>
+      )}
 
       <div className="browse-recently-posted-container">
         <h1 className="recently-posted-title">Recently Posted</h1>
